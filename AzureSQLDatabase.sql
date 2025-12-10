29b24eb4c7910f7f2bb3f7cf6110970e9588179b
pdottroendle <pdottroendle@gmail.com>
2025-12-09 20:37:03 -0800

    Create users, roles, and database tables
    Added users and assigned roles for database access. Created tables for Teams, Players, and PerformanceStats.

---
+++ AzureSQLDatabase.sql
@@ -0,0 +1,123 @@
+
+CREATE USER [professor@bellevuecollege.edu] FROM EXTERNAL PROVIDER;
+CREATE USER [sean.miles@bellevuecollege.edu] FROM EXTERNAL PROVIDER;
+CREATE USER [miles.griffith@bellevuecollege.edu] FROM EXTERNAL PROVIDER;
+CREATE USER [usman.rizvi@bellevuecollege.edu] FROM EXTERNAL PROVIDER;
+
+ALTER ROLE db_datareader ADD MEMBER [professor@bellevuecollege.edu];
+ALTER ROLE db_datawriter ADD MEMBER [professor@bellevuecollege.edu];
+
+ALTER ROLE db_datareader ADD MEMBER [sean.miles@bellevuecollege.edu];
+ALTER ROLE db_datawriter ADD MEMBER [sean.miles@bellevuecollege.edu];
+
+ALTER ROLE db_datareader ADD MEMBER [miles.griffith@bellevuecollege.edu];
+ALTER ROLE db_datawriter ADD MEMBER [miles.griffith@bellevuecollege.edu];
+
+ALTER ROLE db_datareader ADD MEMBER [usman.rizvi@bellevuecollege.edu];
+ALTER ROLE db_datawriter ADD MEMBER [usman.rizvi@bellevuecollege.edu];
+
+
+
+--------------------------------------------
+-- Teams (owned by Team Management Service)
+--------------------------------------------
+IF OBJECT_ID(N'dbo.Teams', N'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.Teams (
+        teamId       UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+        teamName     NVARCHAR(100)    NOT NULL,
+        createdDate  DATETIME2(0)     NOT NULL DEFAULT SYSUTCDATETIME(),
+        CONSTRAINT PK_Teams PRIMARY KEY (teamId)
+    );
+END;
+
+--------------------------------------------
+-- Players (owned by Player Management Service)
+--------------------------------------------
+IF OBJECT_ID(N'dbo.Players', N'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.Players (
+        playerId     UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+        playerName   NVARCHAR(100)    NOT NULL,
+        position     NVARCHAR(50)     NULL,
+        teamId       UNIQUEIDENTIFIER NULL,  -- NULL = undrafted
+        CONSTRAINT PK_Players PRIMARY KEY (playerId),
+        CONSTRAINT FK_Players_Teams_teamId FOREIGN KEY (teamId) REFERENCES dbo.Teams(teamId)
+    );
+
+    CREATE INDEX IX_Players_teamId ON dbo.Players(teamId);
+END;
+
+--------------------------------------------
+-- PerformanceStats (owned by Performance Tracking Service)
+--------------------------------------------
+IF OBJECT_ID(N'dbo.PerformanceStats', N'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.PerformanceStats (
+        statId          UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+        playerId        UNIQUEIDENTIFIER NOT NULL,
+        points          INT              NOT NULL,
+        assists         INT              NOT NULL,
+        rebounds        INT              NOT NULL,
+        gameDate        DATETIME2(0)     NOT NULL DEFAULT SYSUTCDATETIME(),
+        competitionName NVARCHAR(100)    NULL,
+        CONSTRAINT PK_PerformanceStats PRIMARY KEY (statId),
+        CONSTRAINT FK_PerformanceStats_Players_playerId FOREIGN KEY (playerId) REFERENCES dbo.Players(playerId)
+    );
+
+    CREATE INDEX IX_Perf_Player_Date ON dbo.PerformanceStats(playerId, gameDate DESC);
+END;
+=======
+
+--------------------------------------------
+-- Teams (owned by Team Management Service)
+--------------------------------------------
+IF OBJECT_ID(N'dbo.Teams', N'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.Teams (
+        teamId       UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+        teamName     NVARCHAR(100)    NOT NULL,
+        createdDate  DATETIME2(0)     NOT NULL DEFAULT SYSUTCDATETIME(),
+        CONSTRAINT PK_Teams PRIMARY KEY (teamId)
+    );
+END;
+
+--------------------------------------------
+-- Players (owned by Player Management Service)
+--------------------------------------------
+IF OBJECT_ID(N'dbo.Players', N'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.Players (
+        playerId     UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+        playerName   NVARCHAR(100)    NOT NULL,
+        position     NVARCHAR(50)     NULL,
+        teamId       UNIQUEIDENTIFIER NULL,  -- NULL = undrafted
+        CONSTRAINT PK_Players PRIMARY KEY (playerId),
+        CONSTRAINT FK_Players_Teams_teamId FOREIGN KEY (teamId) REFERENCES dbo.Teams(teamId)
+    );
+
+    CREATE INDEX IX_Players_teamId ON dbo.Players(teamId);
+END;
+
+--------------------------------------------
+-- PerformanceStats (owned by Performance Tracking Service)
+--------------------------------------------
+IF OBJECT_ID(N'dbo.PerformanceStats', N'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.PerformanceStats (
+        statId          UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+        playerId        UNIQUEIDENTIFIER NOT NULL,
+        points          INT              NOT NULL,
+        assists         INT              NOT NULL,
+        rebounds        INT              NOT NULL,
+        gameDate        DATETIME2(0)     NOT NULL DEFAULT SYSUTCDATETIME(),
+        competitionName NVARCHAR(100)    NULL,
+        CONSTRAINT PK_PerformanceStats PRIMARY KEY (statId),
+        CONSTRAINT FK_PerformanceStats_Players_playerId FOREIGN KEY (playerId) REFERENCES dbo.Players(playerId)
+    );
+
+    CREATE INDEX IX_Perf_Player_Date ON dbo.PerformanceStats(playerId, gameDate DESC);
+END;
+
+
+
